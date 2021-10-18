efd59ef86d39b1bf4fb8aa56e839086d0007bae3
Sergio Jurado <sergio.jurado@jaraxa.com>
2021-10-18 15:46:22 +0200

    changed cabal build steps

--- scripts/cabal-build.sh
+++ scripts/cabal-build.sh
@@ -5,20 +5,18 @@ set +x
 
 # executes cabal build all
 # parses executables created from compiler output and copies it to ~./cabal/bin folder.
-source /tekton/home/.ghcup/env
 
-export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH"
+cabal update -j
 
 git clone https://github.com/input-output-hk/cardano-node.git && cd cardano-node
 git fetch --all --recurse-submodules --tags
 git checkout tags/$VERSION
 
-cabal configure --with-compiler=ghc-8.10.4
+cabal configure -j --with-compiler=ghc-8.10.4
 
 echo "Running cabal update to ensure you're on latest dependencies.."
 echo "Building..."
-cabal build $PACKAGES 2>&1 | tee /tmp/build.log
+cabal build -j $PACKAGES 2>&1 | tee /tmp/build.log
 
 grep "^Linking" /tmp/build.log | grep -Ev 'test|golden|demo' | while read -r line ; do
     act_bin_path=$(echo "$line" | awk '{print $2}')
--- scripts/prereqs.sh
+++ scripts/prereqs.sh
@@ -1,27 +1,11 @@
 #!/usr/bin/env bash
 set +x
+
+# Building by default by a tekton task using docker oficial haskell:8.10.4 image.
+# If Building on any other Debian based distro you will need to uncomment the following
+
 apt-get update -y
 apt-get install curl automake build-essential pkg-config libffi-dev libgmp-dev libssl-dev libtinfo-dev libsystemd-dev zlib1g-dev make g++ tmux git jq wget libncursesw5 libtool autoconf -y
-
-wget https://downloads.haskell.org/~cabal/cabal-install-3.4.0.0/cabal-install-3.4.0.0-x86_64-ubuntu-16.04.tar.xz
-tar -xf cabal-install-3.4.0.0-x86_64-ubuntu-16.04.tar.xz
-rm cabal-install-3.4.0.0-x86_64-ubuntu-16.04.tar.xz
-mkdir -p $HOME/.local/bin
-mv cabal $HOME/.local/bin/
-
-export PATH="~/.local/bin:$PATH"
-
-cabal update && cabal --version
-
-curl -s -m 60 --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sed -e 's#read.*#answer=Y;next_answer=P;hls_answer=N#' | bash
-
-source /tekton/home/.ghcup/env
-ghcup install ghc 8.10.4
-ghcup install cabal 3.4.0.0
-ghcup set ghc 8.10.4
-ghcup set cabal 3.4.0.0
-
-mkdir -p ~/src
 
 git clone https://github.com/input-output-hk/libsodium && cd libsodium
 git checkout 66f017f1
